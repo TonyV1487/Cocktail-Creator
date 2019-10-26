02110faa76c1b664470ac14f784ec4a9a7ee6401
TonyV <tvester1487@gmail.com>
2019-10-26 11:22:19 -0500

    Display checkboxes for liquor and ingredients

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+  var liquorList = ['test1', 'test2'];
+  var ingredientList = ['ing1', 'ing2'];
+
+  function displayLiquor() {
+    for (i = 0; i < liquorList.length; i++) {
+      var liquorItem = liquorList[i];
+      $('#liquor').append(`<input type="checkbox">${liquorItem}<br>`);
+      console.log(liquorItem);
+    }
+  }
+
+  function displayIngredients() {
+    for (i = 0; i < ingredientList.length; i++) {
+      var ingredientItem = ingredientList[i];
+      $('#ingredients').append(`<input type="checkbox">${ingredientItem}<br>`);
+      console.log(ingredientItem);
+    }
+  }
+
   function searchIngredient(ingredient) {
     var queryUrl =
       'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient;
@@ -14,4 +33,6 @@ $(document).ready(function() {
     var ingredient = $('#userIngredient').val();
     searchIngredient(ingredient);
   });
+  displayLiquor();
+  displayIngredients();
 });
