fea9a92c70a05a3d507786f0da493898b1aed330
TonyV <tvester1487@gmail.com>
2019-10-26 13:22:02 -0500

    Add carets to liquor and ingredients

--- script.js
+++ script.js
@@ -171,4 +171,15 @@ $(document).ready(function() {
   });
   displayLiquor();
   displayIngredients();
+
+  // Caret
+  var toggler = document.getElementsByClassName('caret');
+  var i;
+
+  for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener('click', function() {
+      this.parentElement.querySelector('.nested').classList.toggle('active');
+      this.classList.toggle('caret-down');
+    });
+  }
 });
